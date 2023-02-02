6206ec7aa6d2eab32387661c180569dc6d15b592
Jakub Szefer <jakub.szefer@yale.edu>
2023-02-02 15:43:01 -0500

    Adding basic script to query OpenAI for answers to questions stored in JSON file.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # openai-eeng201-solution-generator
+
 Experimenting with using OpenAI to generate solution to homework, quiz, lab, and exam questions in EENG 201.
+
+# Installation Instrucitons
+
+python3 -m pip install openai
+
---
+++ generate-quiz-solutions.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import json
+import openai
+import argparse
+import requests
+
+# Setup argument parser
+parser = argparse.ArgumentParser(description='Generate solutions to a quiz, input is JSON file with quiz questions.',
+                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-s', '--settings', dest='settings', type=str, required=True,
+          help='Path to JSON settings file with OpenAI API key.')
+parser.add_argument('-q', '--questions', dest='questions', type=str, required=True,
+          help='Path to JSON settings file with questions.')
+args = parser.parse_args()
+
+# Read API key from JSON file
+with open(args.settings, "r") as settings_file:
+    data = json.load(settings_file)
+
+apikey = data['apikey']
+organization = data['organization']
+
+# Connect to OpenAI
+openai.organization = organization
+openai.api_key = apikey
+
+# Load questions
+with open(args.questions, "r") as questions_file:
+    data = json.load(questions_file)
+
+for question in data:
+    print("")
+    print(question)
+    print(data[question])
+
+    prompt = data[question]
+
+    text = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        max_tokens=256,
+        temperature=1,
+        n=1
+    )
+
+    for choice in text['choices']:
+        print(choice['text'])
+
+# Done
+print("Finished.")
