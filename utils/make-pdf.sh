67550b120d23388385e693fb6791a147e99b7692
Jakub Szefer <jakub.szefer@yale.edu>
2023-03-11 23:52:19 -0500

    Add bash script to batch generate PDFs of solutions.

---
+++ utils/make-pdf.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-02-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-03-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-04-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-05-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-06-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-01.json -o ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-02.json -o ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-03.json -o ../solutions-openai-generated/quizzes/quiz-week-07-solutions-set-03.pdf
+
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-01.json -o ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-02.json -o ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-03.json -o ../solutions-openai-generated/midterm-exam/midterm-exam-solutions-set-03.pdf
+
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-01-solutions-set-01.json -o ../solutions-openai-generated/labs/lab-01-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-01-solutions-set-02.json -o ../solutions-openai-generated/labs/lab-01-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-01-solutions-set-03.json -o ../solutions-openai-generated/labs/lab-01-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-02-solutions-set-01.json -o ../solutions-openai-generated/labs/lab-02-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-02-solutions-set-02.json -o ../solutions-openai-generated/labs/lab-02-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-02-solutions-set-03.json -o ../solutions-openai-generated/labs/lab-02-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-03-solutions-set-01.json -o ../solutions-openai-generated/labs/lab-03-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-03-solutions-set-02.json -o ../solutions-openai-generated/labs/lab-03-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-03-solutions-set-03.json -o ../solutions-openai-generated/labs/lab-03-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-04-solutions-set-01.json -o ../solutions-openai-generated/labs/lab-04-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-04-solutions-set-02.json -o ../solutions-openai-generated/labs/lab-04-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/labs/lab-04-solutions-set-03.json -o ../solutions-openai-generated/labs/lab-04-solutions-set-03.pdf
+
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-01.json -o ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-02.json -o ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-03.json -o ../solutions-openai-generated/lab-practical-exam/lab-practical-exam-solutions-set-03.pdf
+
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-01-solutions-set-01.json -o ../solutions-openai-generated/homeworks/assignment-01-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-01-solutions-set-02.json -o ../solutions-openai-generated/homeworks/assignment-01-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-01-solutions-set-03.json -o ../solutions-openai-generated/homeworks/assignment-01-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-02-solutions-set-01.json -o ../solutions-openai-generated/homeworks/assignment-02-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-02-solutions-set-02.json -o ../solutions-openai-generated/homeworks/assignment-02-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-02-solutions-set-03.json -o ../solutions-openai-generated/homeworks/assignment-02-solutions-set-03.pdf
+
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-03-solutions-set-01.json -o ../solutions-openai-generated/homeworks/assignment-03-solutions-set-01.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-03-solutions-set-02.json -o ../solutions-openai-generated/homeworks/assignment-03-solutions-set-02.pdf
+python3 generate-latex-pdf-from-json-solution-files.py -i ../solutions-openai-generated/homeworks/assignment-03-solutions-set-03.json -o ../solutions-openai-generated/homeworks/assignment-03-solutions-set-03.pdf
+
